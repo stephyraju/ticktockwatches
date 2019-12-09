48d63c5fd10f929f5fed4ada7477a5124435e369
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2019-12-09 10:33:28 +0000

    logout functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
+def logout(request):
+    def logout(request):
+    '''Log the user out / request contains the user object'''
+    auth.logout(request)
+    messages.success(request,'You have been succesfully loged out')
+    return redirect (reverse('index'))
