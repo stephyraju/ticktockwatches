0d175a24ada3c4ab7f932a9ba275033bbd903e3b
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2019-12-09 10:48:03 +0000

    messages added

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 # Create your views here.
+
+def index(request):
+    '''Return the home page'''
+    return render(request, 'index.html')
+
 def logout(request):
-    def logout(request):
-    '''Log the user out / request contains the user object'''
+    '''Log out the user / request contains the user object'''
     auth.logout(request)
     messages.success(request,'You have been succesfully loged out')
     return redirect (reverse('index'))
