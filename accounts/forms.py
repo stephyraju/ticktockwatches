b859770e232b35a4a768d1bc4ba8a7a0f5500792
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2019-12-14 07:05:26 +0000

    Add registration functionality

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,32 +14,72 @@ class UserLoginForm(forms.Form):
 class UserRegistrationForm(UserCreationForm):
     """Form used to register a new user"""
 
+    first_name = forms.CharField(
+        widget=forms.TextInput)
+    last_name = forms.CharField(
+        widget=forms.TextInput)
+    username = forms.CharField(
+        widget=forms.TextInput)
+    email = forms.CharField(
+        widget=forms.TextInput)
     password1 = forms.CharField(
         label="Password",
         widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label="Password Confirmation",
+        label="Confirm the password", 
         widget=forms.PasswordInput)
-    
+           
+
+    ''' Fields that display in the form '''
+
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['first_name','last_name','email', 'username', 'password1', 'password2']
     
+    '''validation of first_name'''
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        
+        if not first_name:
+            raise forms.ValidationError(u'This field is required.')
+        return first_name
+
+
+    '''validation of last_name'''
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        
+        if not last_name:
+            raise forms.ValidationError(u'This field is required.')
+        return last_name
+
+
+    '''validation of username'''
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        return username
+
+    '''validation of email'''
     def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
+
+        if not email:
+            raise forms.ValidationError(u'This field is required.')
+
         if User.objects.filter(email=email).exclude(username=username):
             raise forms.ValidationError(u'Email address must be unique')
         return email
 
+    '''validation of passwords'''
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
 
         if not password1 or not password2:
-            raise ValidationError("Please confirm your password")
+            raise forms.ValidationError(u'Please confirm your password')
         
         if password1 != password2:
-            raise ValidationError("Passwords must match")
+            raise forms.ValidationError(u'The passwords must match')
         
         return password2
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -36,3 +36,32 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+
+def registration(request):
+    '''Registration page'''
+   
+    '''If the user is already logged in it redirects them to the index page'''
+
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(request=request, user=user)
+                messages.success(request, 'You have been succesfully registered')
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    
+        
+    else:
+            registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+            "registration_form": registration_form})
