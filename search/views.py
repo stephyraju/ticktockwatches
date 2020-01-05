880cf9369a62c37abb9afe475015b13ac9ed34a2
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-05 06:00:36 +0530

    Create Search views

--- search/views.py
+++ search/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
+def do_search(request):
+    products = Product.objects.filter(name__contains=request.GET['q'])
+    return render(request, "allproducts.html", {"products": products})
