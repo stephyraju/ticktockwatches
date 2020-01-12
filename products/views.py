f80f72af052d97901d6a3821f09dfd0ee009cc38
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-13 04:59:42 +0530

    Add Product views for categories

--- products/views.py
+++ products/views.py
@@ -9,3 +9,33 @@ def all_products(request):
 def all_products2(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+def view_menswatch(request):
+    """View to display only Mens"""
+    products = Product.objects.all().filter(category='Mens Watches')
+    return render(request, "products.html", {"products": products})
+
+def view_ladieswatch(request):
+    """View to display only Ladies Watches"""
+    products = Product.objects.all().filter(category='Ladies Watches')
+    return render(request, "products.html", {"products": products})
+
+def view_kidswatch(request):
+    """View to display only Kids Watches"""
+    products = Product.objects.all().filter(category='Kids Watches')
+    return render(request, "products.html", {"products": products})
+
+def view_rolex(request):
+    """View to display only Rolex"""
+    products = Product.objects.all().filter(category='Rolex')
+    return render(request, "products.html", {"products": products})
+
+def view_timex(request):
+    """View to display only Timex"""
+    products = Product.objects.all().filter(category='Timex')
+    return render(request, "products.html", {"products": products})
+
+def view_tedbaker(request):
+    """View to display only Ted Baker"""
+    products = Product.objects.all().filter(category='Ted Baker')
+    return render(request, "products.html", {"products": products})
