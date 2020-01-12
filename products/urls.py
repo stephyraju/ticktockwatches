8caa8f705aeb833ab44c499b601af6e5a5dd57ae
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-13 05:00:46 +0530

    Add  urls for categories

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_products, all_products2
+from .views import all_products, all_products2, view_menswatch
 
 
 urlpatterns = [
     url(r'^$', all_products, name='index'),
-    url(r'^$', all_products2, name='all_products2')
+    url(r'^$', all_products2, name='all_products2'),
+    url(r'^view_menswatch/$', view_menswatch, name='menswatch')
 ]
