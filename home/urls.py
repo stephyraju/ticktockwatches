d00ead20d15946fb2f02fcdd56ff26180b10a1f1
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-19 19:55:48 +0000

    Add views and urls for delivery page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import  view_about 
+from .views import  view_about, view_delivery
 
 urlpatterns = [
-    url(r'^about/$', view_about, name='about')
+    url(r'^about/$', view_about, name='about'),
+    url(r'^delivery/$', view_delivery, name='delivery'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-# # Create your views here.
+ # Create your views here.
 def view_about(request):
     """A View that renders the about page"""
     return render(request, "about.html")
+
+def view_delivery(request):
+    """A View that renders the delivery page"""
+    return render(request, "delivery.html")
