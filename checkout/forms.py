61fdb01756d6463519f34c9f7c7a6c24bb9f83d3
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-22 06:28:12 +0000

    Checkout form modified

--- checkout/forms.py
+++ checkout/forms.py
@@ -18,7 +18,5 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2',
-            'county'
-            )
+            'full_name', 'phone_number', 'street_address1',  'street_address2', 'town_or_city', 'county', 'postcode','country'
+             )
