31ec6f0bc5b77f9c9cc0911ec6d85858f01203b5
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-23 12:26:45 +0000

    Add favourite models and views

--- favourites/models.py
+++ favourites/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class User(models.Model):
+#     user = models.CharField(max_length=30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return "%s %s" % (self.user)
+
+class Favourites(models.Model):
+
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    product =  models.ForeignKey(Product, null=False)
+
+    def __str__(self):
+        return self.favourites
+
+    class Meta:
+        ordering = ['-id']
+
+    
--- favourites/views.py
+++ favourites/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from products.models import Product
 from django.http import HttpResponseRedirect
+from .models import Favourites
 from django.contrib.auth.decorators import login_required
 
 """
@@ -14,7 +15,6 @@ If the Product is not in favourite then save it as favourite
 @login_required
 def add_remove_favourites(request, id):
     product = get_object_or_404(Product,pk=id)
-
     if request.method == 'POST':
         user_profile = User.objects.get(email=request.user.email)
         fav_product = Product.objects.get(pk=id)
@@ -24,4 +24,12 @@ def add_remove_favourites(request, id):
         else:
             user_profile.favourites.add(fav_product)
 
+    
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    
+    #  product = get_object_or_404(Product, id=id)
+    #  if product.favourite.filter(id=reqest.user.id).exists():
+    #       products.favourite.remove(request.user)
+    #  else:
+    #     product.favourite.add(request.user)
+    #  return HttpResponseRedirect(product.get_absolute_url())
