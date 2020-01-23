84997a7eda684f7aff787d72770ec97280ca4156
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-23 14:00:15 +0000

    Modified favourite views

--- favourites/views.py
+++ favourites/views.py
@@ -14,22 +14,31 @@ If the Product is not in favourite then save it as favourite
 """    
 @login_required
 def add_remove_favourites(request, id):
-    product = get_object_or_404(Product,pk=id)
-    if request.method == 'POST':
-        user_profile = User.objects.get(email=request.user.email)
-        fav_product = Product.objects.get(pk=id)
+
+    # product = get_object_or_404(Product,pk=id)
+    # if request.method == 'POST':
+    #     user_profile = User.objects.get(email=request.user.email)
+    #     fav_product = Product.objects.get(pk=id)
         
-        if fav_product in user_profile.favourites.all():
-            user_profile.favourites.remove(fav_product)
-        else:
-            user_profile.favourites.add(fav_product)
+    #     if fav_product in user_profile.favourites.all():
+    #         user_profile.favourites.remove(fav_product)
+    #     else:
+    #         user_profile.favourites.add(fav_product)
 
     
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    # return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     
-    #  product = get_object_or_404(Product, id=id)
-    #  if product.favourite.filter(id=reqest.user.id).exists():
-    #       products.favourite.remove(request.user)
-    #  else:
-    #     product.favourite.add(request.user)
+    product = get_object_or_404(Product, id=id)
+    user_profile = User.objects.get(email=request.user.email)
+    new_favourite = Favourite(user=user_profile, product=product)
+    favourites.add(new_favourite)
+
+    if favourite.filter(user=user_profile).exists():
+        favourite.remove(request.user)
+    else:
+        favourite.add(request.user)
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     #  return HttpResponseRedirect(product.get_absolute_url())
+     
+    
