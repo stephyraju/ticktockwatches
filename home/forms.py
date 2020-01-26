9cd76543fab66860a09685a36657abfbcd53c664
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-26 19:08:09 +0000

    Modified contact form and view

--- home/forms.py
+++ home/forms.py
@@ -1,22 +1,14 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    """ 
-    Form for contact page 
-    """
     
-    name = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                "rows": 5,
-            },
-        ),
-    )
-    class Meta:
-        fields = [
-            'name',
-            'email',
-            'message',
-        ]
+    """
+    Form for contact page
+    """
+
+    name = forms.CharField(required=True, max_length=75)
+    email = forms.EmailField(required=True, max_length=75)
+    subject = forms.CharField(required=True, max_length=75)
+    message = forms.CharField(widget=forms.Textarea, required=True, 
+                              max_length=500
+                             ) 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.core.mail import send_mail
 from django.utils import timezone
 from django.conf import settings
-from django.core.mail import send_mail
 from .forms import ContactForm
 from django.contrib import auth, messages
 
@@ -23,36 +23,30 @@ def view_faqs(request):
     return render(request, "faqs.html")
 
 def view_contact(request):
-    '''Returns the contact us form page'''
-    if request.user.is_authenticated:
-        initial_data = {
-            'name': request.user.first_name,
-            'email': request.user.email
-        }
-
-        contact_form = ContactForm(initial=initial_data)
-        
-        if request.method == 'POST':
-            contact_form = ContactForm(request.POST)
-
-            if contact_form.is_valid():
-                message = request.POST['message']
-                email_from = request.user.email
-                recipient_list = ['stephyraju@gmail.com']
-                send_mail ( message, email_from, recipient_list)
-                
-                fail_silently=False
-                
-                messages.success(request, 
-                                "Your message has been sent!",
-                                extra_tags="alert-success")
-                return redirect(reverse('contact'))
+    """View handle contact form requests"""
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            message = request.POST['message']
+            subject = request.POST['subject']
+            send_mail(
+                subject,
+                "Message from: " +
+                request.POST['email'] +
+                "Message: " +
+                message,
+                'from@example.com',
+                ['stephyraju@gmail.com'],
+                fail_silently=False,
+            )
+            messages.success(request, 
+                             "Your message has been sent successfully!",
+                             extra_tags="alert-success")
+            return redirect(reverse('index'))
         else:
             messages.error(request, "Unable to send message at this time",
                                     extra_tags="alert-danger")
     else:
         contact_form = ContactForm()
+    return render(request, 'contact.html', {'contact_form': contact_form})
    
-    print()
-    return render(request, 'contact.html', {'contact_form':contact_form})
-    
