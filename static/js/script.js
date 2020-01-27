f20ec78a5bf1db73d3d0dfc0d9eb1217137bdf6a
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-27 06:51:26 +0000

    Add message timeout function

--- static/js/script.js
+++ static/js/script.js
@@ -2,27 +2,17 @@ $(document).ready(function() {
     // messages timeout for 10 sec 
     setTimeout(function() {
         $('.messages').fadeOut('slow');
-    }, 10000); // <-- time in milliseconds, 1000 =  1 sec
-
-//     // delete message
-//     $('.del-msg').live('click',function(){
-//         $('.del-msg').parent().attr('style', 'display:none;');
-//     })
+    }, 4000);
 });
+
+// Search icon display
 function myFunction() {
 
  var x = document.getElementById("searchDiv");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
+  } 
+  else {
     x.style.display = "none";
   }
-    
-    /*if (document.getElementById("form").style.display === "none") {
-        
-        document.getElementById("form").style.display = "block";
-        } 
-    else {
-        document.getElementById("form").style.display = "none";
-    }*/
-}
+};
