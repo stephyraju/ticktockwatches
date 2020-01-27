ff8347c8a4aff47a56496b00d9efebb1c051275c
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-27 06:51:57 +0000

    Add static path

--- ticktockwatches/settings.py
+++ ticktockwatches/settings.py
@@ -135,16 +135,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
