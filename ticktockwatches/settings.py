dee061e7d11e9efc428eae9d749c26a7ff83a19c
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-28 17:42:16 +0000

    Database url for dev removed

--- ticktockwatches/settings.py
+++ ticktockwatches/settings.py
@@ -88,26 +88,22 @@ WSGI_APPLICATION = 'ticktockwatches.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# in development
-if development==True:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-# in production
-else:
-    if "DATABASE_URL" in os.environ:
-        DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    else:
-        print("Database URL not found. Using SQLite instead")
-        DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
     
 
 
