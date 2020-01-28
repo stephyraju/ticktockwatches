48010a44d568eaf3984b27a17412f9e95743210a
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-01-28 19:11:17 +0000

    Product test modified

--- products/tests.py
+++ products/tests.py
@@ -5,6 +5,8 @@ from .models import Product
 class ProductTests(TestCase):
   
     def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+        # test_name = Product(name='A product')
+        # self.assertEqual(str(test_name), 'A product')
 
+         product = Product(title="Test TickTock")
+         self.assertEqual(str(product), product.title)
