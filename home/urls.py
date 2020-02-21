994ed0fe2513671ecad3657c7cfa2443d30a8328
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-02-21 17:32:53 +0000

    Typos corrected

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^about/$', view_about, name='about'),
     url(r'^delivery/$', view_delivery, name='delivery'),
     url(r'^return/$', view_return, name='return'),
-    url(r'^faqs$', view_faqs, name='faqs'),
-    url(r'^contact$', view_contact, name='contact')
+    url(r'^faqs/$', view_faqs, name='faqs'),
+    url(r'^contact/$', view_contact, name='contact')
 ]
