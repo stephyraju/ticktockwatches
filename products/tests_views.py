9d4fa568ba11be6d2d55d1d56e2c9a4502e55f46
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-02-24 23:36:50 +0000

    Add tests to products

--- products/tests_views.py
+++ products/tests_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from .models import Product
 
 c=Client()
 
@@ -11,6 +12,62 @@ class TestProductViews(TestCase):
         self.assertTemplateUsed(page, "index.html")
 
     def test_all_products_page(self):
-        page = c.get("/products/products")
+        page = c.get("/products_list/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "products.html")
+    
+    def test_kids_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_kidswatch/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_mens_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_menswatch/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_ladies_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_ladieswatch/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_rolex_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_rolex/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_timex_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_timex/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_tedbaker_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_tedbaker/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_tissot_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_tissot/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_michaelkores_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_michaelkores/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_lorus_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_lorus/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_lacoste_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_lacoste/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
+    def test_citizen_watch_redirects_to_products_page(self):
+        page = c.get("/products/view_citizen/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+
