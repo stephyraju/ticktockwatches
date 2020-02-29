07a4bdae5c399f16d1cde9cdcab7d0a786f34205
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-02-29 08:24:43 +0000

    Environment variables set for allowed host

--- ticktockwatches/settings.py
+++ ticktockwatches/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME'), 'ticktockwatches.herokuapp.com',] 
+ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME')] 
 
 # if development: 
 # ALLOWED_HOSTS = ['0.0.0.0']
