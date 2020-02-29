bffd2ee802b71b29f2568eb66424b9ea01ea3478
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-02-29 22:54:37 +0000

    Debug changed to False

--- ticktockwatches/settings.py
+++ ticktockwatches/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME')] 
-# ALLOWED_HOSTS = ['0.0.0.0']
+#ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
